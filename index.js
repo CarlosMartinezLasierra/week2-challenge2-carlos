49e334a574736db23905a6a310ca78fed004000c
Carlos Martinez <carlosmartinezlasierra@gmail.com>
2023-05-02 18:46:57 +0200

    Add principal npm setting as husky and jest

---
+++ index.js
@@ -0,0 +1,8 @@
+let valueA ()
+let valueB ()
+console.log(object.is(valueA, valueB));
+console.log(object.is(valueA, valueB));
+console.log(object.is(valueA, valueB));
+console.log(object.is(valueA, valueB));
+console.log(object.is(valueA, valueB));
+console.log(object.is(valueA, valueB));
